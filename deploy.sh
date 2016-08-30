4ad4dc24963ad178a957eeea861708dab938d675
Thomas Couchoud <zerderr@gmail.com>
2016-08-30 15:54:57 +0200

    Try travis deploy

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+gitLastCommit=$(git show --summary --grep="Merge pull request")
+if [[ -z "$gitLastCommit" ]]
+then
+	lastCommit=$(git log --format="%H" -n 1)
+else
+	echo "We got a Merge Request!"
+	arr=($gitLastCommit)
+	lastCommit=${arr[4]}
+fi
+echo $lastCommit
+
+filesChanged=$(git diff-tree --no-commit-id --name-only -r $lastCommit)
+if [ ${#filesChanged[@]} -eq 0 ]; then
+    echo "No files to update"
+else
+    for f in $filesChanged
+	do
+		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ] && [ "$f" != "README.md" ] && [ "$f" != ".gitignore" ]
+		then
+	 		echo "Uploading $f"
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://ftp.cluster003.ovh.net/www/subdomains/yttrackerdeploy/$f
+		fi
+	done
+fi
