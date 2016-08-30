4641c9ee7226185d41c03c780206a009f4d8779d
Thomas Couchoud <zerderr@gmail.com>
2016-08-30 16:17:11 +0200

    Try to fix deploy again

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(isset($_GET['startPeriod']) && isset($_GET['endPeriod'])){
     <script type="text/javascript" src="js/tablesorter/jquery.tablesorter.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
     <meta charset="UTF-8">
-    <title>aYTTracker</title>
+    <title>YTTracker</title>
 </head>
 <body>
     <div>
