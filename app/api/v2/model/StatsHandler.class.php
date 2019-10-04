c456d5506c4cad7cf1644c5d9620b01389077344
Thomas Couchoud <thomas.couchoud@gmail.com>
2019-10-04 18:04:10 +0200

    Add addUserStat endpoint

--- app/api/v2/model/StatsHandler.class.php
+++ app/api/v2/model/StatsHandler.class.php
@@ -12,8 +12,10 @@
 
 		public function __construct(){ }
 
-		private function getDataType($name){
-			switch($name){
+		private function getDataType($name)
+		{
+			switch($name)
+			{
 				case "opened":
 					return 2;
 				case "watched":
@@ -24,7 +26,8 @@
 
 		private function getUserDurationStats($userUUID, $category, $range = null)
 		{
-			if(!is_int($range)){
+			if(!is_int($range))
+			{
 				$range = $this->DEFAULT_RANGE;
 			}
 
@@ -42,17 +45,20 @@
 		}
 
 		/** @noinspection PhpUnused */
-		public function getUserWatched($groups, $params){
+		public function getUserWatched($groups, $params)
+		{
 			return $this->getUserDurationStats($groups[1], "watched", isset($params["range"]) ? $params['range'] : null);
 		}
 
 		/** @noinspection PhpUnused */
-		public function getUserOpened($groups, $params){
+		public function getUserOpened($groups, $params)
+		{
 			return $this->getUserDurationStats($groups[1], "opened", isset($params["range"]) ? $params['range'] : null);
 		}
 
 		/** @noinspection PhpUnused */
-		public function getUserOpenedCount($groups, $params){
+		public function getUserOpenedCount($groups, $params)
+		{
 			$userUUID = $groups[1];
 			$range = min(3650, isset($params["range"]) ? $params['range'] : $this->DEFAULT_RANGE);
 
@@ -67,7 +73,8 @@
 			return $data;
 		}
 
-		private function getUserSumStats($userUUID, $type, $hours){
+		private function getUserSumStats($userUUID, $type, $hours)
+		{
 			$prepared = DBConnection::getConnection()->prepare("SELECT SUM(`Stat`)/1000 AS Total FROM `YTT_Records` WHERE `Type`=:type AND `UUID`=:uuid AND DATE(`YTT_Records`.`Time`) >= DATE_SUB(NOW(), INTERVAL :hours HOUR)");
 			$prepared->execute(array(":uuid" => $userUUID, ':hours' => $hours, ':type' => $this->getDataType($type)));
 			if($row = $prepared->fetch())
@@ -77,7 +84,8 @@
 			return 0;
 		}
 
-		private function getUserCountStats($userUUID, $hours){
+		private function getUserCountStats($userUUID, $hours)
+		{
 			$prepared = DBConnection::getConnection()->prepare("SELECT COUNT(*) AS Total FROM `YTT_Records` WHERE `Type`=:type AND `UUID`=:uuid AND DATE(`YTT_Records`.`Time`) >= DATE_SUB(NOW(), INTERVAL :hours HOUR) AND Stat > 0");
 			$prepared->execute(array(":uuid" => $userUUID, ':hours' => $hours, ':type' => $this->getDataType('opened')));
 			if($row = $prepared->fetch())
@@ -88,25 +96,43 @@
 		}
 
 		/** @noinspection PhpUnused */
-		public function getUserStats($groups, $params){
+		public function getUserStats($groups, $params)
+		{
 			$userUUID = $groups[1];
 
-			$data = array(
-				'total' => array(
-					'opened' => $this->getUserSumStats($userUUID, 'opened', 24 * $this->MAX_RANGE),
-					'watched' => $this->getUserSumStats($userUUID, 'watched', 24 * $this->MAX_RANGE),
-					'count' => $this->getUserCountStats($userUUID, 24 * $this->MAX_RANGE)
-				),
-				'week' => array(
-					'opened' => $this->getUserSumStats($userUUID, 'opened', 24 * 7),
-					'watched' => $this->getUserSumStats($userUUID, 'watched', 24 * 7),
-					'count' => $this->getUserCountStats($userUUID, 24 * 7)
-				),
-				'day' => array(
-					'opened' => $this->getUserSumStats($userUUID, 'opened', 24),
-					'watched' => $this->getUserSumStats($userUUID, 'watched', 24),
-					'count' => $this->getUserCountStats($userUUID, 24)
-				));
+			$data = array('total' => array('opened' => $this->getUserSumStats($userUUID, 'opened', 24 * $this->MAX_RANGE), 'watched' => $this->getUserSumStats($userUUID, 'watched', 24 * $this->MAX_RANGE), 'count' => $this->getUserCountStats($userUUID, 24 * $this->MAX_RANGE)), 'week' => array('opened' => $this->getUserSumStats($userUUID, 'opened', 24 * 7), 'watched' => $this->getUserSumStats($userUUID, 'watched', 24 * 7), 'count' => $this->getUserCountStats($userUUID, 24 * 7)), 'day' => array('opened' => $this->getUserSumStats($userUUID, 'opened', 24), 'watched' => $this->getUserSumStats($userUUID, 'watched', 24), 'count' => $this->getUserCountStats($userUUID, 24)));
 			return $data;
+		}
+
+		public function addUserStat($groups, $params)
+		{
+			$userUUID = $groups[1];
+
+			if(!StatsHandler::checkFields($params, ['type', 'videoId', 'stat', 'date']))
+			{
+				return array('code' => 400, 'message' => 'Missing fields');
+			}
+
+			$query = DBConnection::getConnection()->prepare("INSERT IGNORE INTO `YTT_Users`(`UUID`, `Username`) VALUES(:uuid, 'Anonymous');");
+			if(!$query->execute(array(':uuid' => $userUUID)))
+				return array('code' => 400, 'result' => 'err', 'error' => 'E2.1');
+
+			$query2 = DBConnection::getConnection()->prepare("INSERT INTO `YTT_Records`(`UUID`, `Type`, `VideoID`, `Stat`, `Time`, `Browser`) VALUES(:uuid, :type, :videoID, :stat, STR_TO_DATE(:timee, '%Y-%m-%d %H:%i:%s'), :browser);");
+			if(!$query2->execute(array(':uuid' => $userUUID, ':type' => $params['type'], ':videoID' => $params['videoId'], ':stat' => $params['stat'], ':timee' => $this->getTimestamp($params['date']), ':browser' => ($params['browser'] == null ? 'Unknown' : $params['browser']))))
+				return array('code' => 400, 'result' => 'err', 'error' => 'E2.2');
+			return array('code' => 200, 'result' => 'OK');
+		}
+
+		/**
+		 * @param int $date
+		 * @return string
+		 */
+		private function getTimestamp($date)
+		{
+			if(!$date)
+			{
+				return date('%Y-%m-%d %H:%i:%s');
+			}
+			return $date;
 		}
 	}
